f8e6e3db7399cfd7ff33258ccce56ea5b76a854b
RyanBezerra <ryandonascimentobezerra@gmail.com>
2024-05-22 18:18:17 -0300

    first commit

---
+++ ForCareFitness/urls.py
@@ -0,0 +1,7 @@
+from django.urls import path
+from ForCareFitnessApp import views
+
+urlpatterns = [
+    path('', views.cadastro, name="cadastro"),
+    path('login/', views.login, name="login"),
+]
---
+++ ForCareFitnessApp/views.py
@@ -0,0 +1,7 @@
+from django.shortcuts import render
+
+def cadastro(request):
+    return render(request, 'usuarios/cadastro.html')
+
+def login(request):
+    return render(request, 'usuarios/login.html')
